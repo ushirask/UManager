b4a163110c7452ef0ac47ca598a24d93885f5308
ushirask <ushira.16@cse.mrt.ac.lk>
2019-02-11 22:50:06 +0530

    polished UI

--- render.js
+++ render.js
@@ -16,8 +16,8 @@ document.getElementById('clbtn').addEventListener('click', _ => {
     readFolder(dir_path);
 })
 
-document.getElementById('back').addEventListener('click', _ => {
-    console.log("current : "+dir_path)
+document.getElementById('home').addEventListener('click', _ => {
+    readFolder('');
 })
 
 function readFolder(path) {
@@ -27,17 +27,17 @@ function readFolder(path) {
     fs.readdir(dir_path, (err, files) => {
         'use strict';
         if (err) throw  err;
-        document.getElementById('listed-files').innerHTML = `<ul id="display-files"></ul>`;
+        document.getElementById('listed-files').innerHTML = `<ul class="list-group list-group-flush" id="display-folders"></ul><ul class="list-group list-group-flush" id="display-files"></ul>`;
         for (let file of files) {
             fs.stat(dir_path + file, (err, stats) => {
                 let theID =(dir_path+file).replace(/ /g,'%20');
                 
                 if (err) throw err;
                 if (stats.isDirectory()) {
-                    document.getElementById('display-files').innerHTML += `<li id=${theID} style="cursor:pointer" ondblclick="readFolder(this.id)"><i class="fa fa-folder-open"></i> ${file}</li>`;
+                    document.getElementById('display-folders').innerHTML += `<li class="list-group-item list-group-item-action" id=${theID} style="cursor:pointer" ondblclick="readFolder(this.id)"><i class="fa fa-folder-open"></i> ${file}</li>`;
                 }
                 else {
-                    document.getElementById('display-files').innerHTML += `<li id=${theID} style="cursor:pointer" ondblclick="openFile(this.id)"><i class="fa fa-file"></i> ${file}</li>`;
+                    document.getElementById('display-files').innerHTML += `<li class="list-group-item list-group-item-action" id=${theID} style="cursor:pointer" ondblclick="openFile(this.id)"><i class="fa fa-file"></i> ${file}</li>`;
                 }
             });
         }
